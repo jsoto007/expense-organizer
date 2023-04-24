669bdf8e6047ef8e0b5ec7777126bf24169a1a92
Jose Soto <jsoto7087@gmail.com>
2023-04-23 23:08:05 -0400

    added create acc

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,15 @@
 class UsersController < ApplicationController
 
   def create
-    
+    user = User.create!(user_params)
+    byebug
+    render json: user
 
+  end 
+
+  private
+
+  def user_params
+    params.permit(:username, :password)
   end 
 end
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,13 +2,27 @@ import React, { useState } from 'react';
 
 
 function Signup() {
+
+  const [currentUser, setCurrentUser] = useState()
   const [signupData, setSignupData] = useState({
     username: "",
     password: ""
   })
 
-  function handleSubmit(e) {
 
+
+  function handleSubmit(e) {
+      e.preventDefault();
+    fetch(`/users`, {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body:JSON.stringify(signupData)
+    })
+    .then(resp => {
+      if(resp.ok){
+        resp.json().then(setCurrentUser)
+      }
+    })
   }
 
 
