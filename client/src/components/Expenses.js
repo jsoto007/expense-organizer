6f656a897ec2b2b47926a406c20095ec9d9f2f57
Jose Soto <jsoto7087@gmail.com>
2023-04-28 19:02:49 -0400

    working with no errors

--- client/src/components/Expenses.js
+++ client/src/components/Expenses.js
@@ -3,21 +3,20 @@ import React, { useState, useEffect } from "react";
 
 function Expenses() {
 
-  const [userExpense, setUserExpense] = useState("")
+  const [userExpense, setUserExpense] = useState([])
 
   useEffect(()=> {
     fetch('/expenses')
     .then(resp => resp.json())
-    .then(setUserExpense, console.log("inside useEffect"))
+    .then(setUserExpense)
   }, [])
 
   console.log(userExpense)
   return (
     <div>
-      {console.log("execute")}
       {userExpense.map((expense) => {
         return (
-          <li>{expense.description}</li>
+          <li key={expense.id}>{expense.description}</li>
         )
       })}
     </div>
