edfa42b5c5650c9953485c8cc934ff052a2dcdab
Jose Soto <jsoto7087@gmail.com>
2023-04-28 22:31:24 -0400

    working well, create acc has error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_many :expenses
   has_many :categories, through: :expenses
 
+  validates :username, presence: true, :uniqueness => true
+  validates :password, presence: true
+
   has_secure_password
 end
