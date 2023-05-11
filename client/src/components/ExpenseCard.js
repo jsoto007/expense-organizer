64f0e8df4b983f8a81891bf36f1efb4b08cf1616
Jose Soto <jsoto7087@gmail.com>
2023-05-11 00:08:53 -0400

    displays categories under expenses

--- client/src/components/ExpenseCard.js
+++ client/src/components/ExpenseCard.js
@@ -9,6 +9,7 @@ function ExpenseCard( { expense } ) {
       <ul className="expenses-li">
         <li><b>Description | </b>{expense.description}</li>
         <li><b>Amount | </b>{expense.amount}</li>
+        <li id="category-li"><b>Category | </b>{expense.category.name}</li>
       </ul>
      
     </div>
