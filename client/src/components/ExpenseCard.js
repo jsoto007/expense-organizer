8fc0543302cd1f9518cb48a87735b203b11919c9
Jose Soto <jsoto7087@gmail.com>
2023-05-15 15:27:32 -0400

    working with up errors

--- client/src/components/ExpenseCard.js
+++ client/src/components/ExpenseCard.js
@@ -5,7 +5,7 @@ import EditExpense from "./EditExpense";
 
 function ExpenseCard( { expense } ) {
   
-  const [toggleEdit, setToggleEdit] = useState(false)
+  const [toggleEdit, setToggleEdit] = useState(true)
 
   function handleToggleEdit() {
       setToggleEdit((toggleEdit) => !toggleEdit)
@@ -13,17 +13,19 @@ function ExpenseCard( { expense } ) {
 
   return (
     <div>
-      { !toggleEdit ? (
-        <>
+      {toggleEdit ? (
+        <div className="expense-div">
         <h3>{expense.summary}</h3>
         <ul className="expenses-li">
           <li><b>Description | </b>{expense.description}</li>
           <li><b>Amount | </b>{expense.amount}</li>
           <li id="category-li"><b>Category | </b>{expense.category.name}</li>
         </ul>
-        <DeleteExpense expense={expense} />
-        <button onClick={handleToggleEdit}>✏️</button>
-        </>
+        <div className="delete-and-edit">
+          <DeleteExpense expense={expense} />
+          <button onClick={handleToggleEdit}>✏️</button>
+        </div>
+        </div>
       ) : (
         <>
         <EditExpense expense={expense} setToggleEdit={setToggleEdit} toggleEdit={toggleEdit}/>
